9e7057db82753e41bac3ddd06989b3c0bad5fdd1
Jason Wang <jasonwang8@berkeley.edu>
2020-11-20 15:14:37 -0800

    working on zip

--- webapp/app.py
+++ webapp/app.py
@@ -1,93 +1,43 @@
 # Importing required libraries, obviously
 import streamlit as st
-import cv2
 from PIL import Image
-import numpy as np
 import os
+import utils
+import zipfile
 
-def detect_bugs(image):
-    #Gray scale and blur the image
-    img = np.array(image.convert('RGB'))
-    imgGray = np.array(image.convert('L'))
-    imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
-    height, width, channels = img.shape
+def page_setup():
+    st.set_page_config(page_title='Pest Counter')
+    st.title("Pest Counter")
+    st.header(":bug: :honeybee: :ant: :beetle: :snail:")
+    st.write("**We count bugs**")
+    st.write("\nUpload an image of a flypaper to count the number of insects")
+    st.write(open("potato.txt").read())
+    utils.set_bg_image('bg.jpg')
 
-    # Setup SimpleBlobDetector parameters.
-    params = cv2.SimpleBlobDetector_Params()
+    #hides hamgurger in upper right and "made with streamlit" footer
+    hide_streamlit_style = """
+        <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            h1, h2, p {
+                text-align:center;
+            }
 
-    # Filter by Area.
-    params.filterByArea = True
-    params.minArea = 300
-    #params.maxArea = 3000
-
-    # Filter by Convexity
-    params.filterByConvexity = True
-    params.minConvexity = 0.01
-
-    # Filter by Inertia
-    params.filterByInertia = True
-    params.minInertiaRatio = 0.01
-
-    # Create a detector with the parameters
-    detector = cv2.SimpleBlobDetector_create(params)
-
-    #detector = cv2.SimpleBlobDetector_create()
-    keypoints = detector.detect(imgBlur)
-    imgKeyPoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    # Crop out keypoints
-    os.mkdir("images")
-    for keypoint in keypoints:
-        filename = 1
-        x = int(keypoint.pt[0])
-        y = int(keypoint.pt[1])
-        size = int(keypoint.size)
-        crop = img[max(1,y-2*size): min(height-1,y+2*size), max(1,x-2*size): min(width-1,x+2*size)]
-        st.image(crop, use_column_width = False)
-        cv2.imwrite("images/" + str(filename) + ".jpg", crop)
-    # Display found keypoints
-    return imgKeyPoints
-
-
-def about():
-	st.write(
-		'''
-		hello there 
-
-		Something
-
-			1. :)
-			2. something else
-			3. bugs
-			4.(\(\  
-			5.( ^.^)  
-			6.(,)(,)  
-
-
-have a nice day
-		''')
-
+        </style>
+    """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 def main():
-    st.title("Pest Counter App :sunglasses: ")
-    st.write("**We count bugs**")
+    page_setup()
 
-    activities = ["Home", "About"]
-    choice = st.sidebar.selectbox("Pick something fun", activities)
+    image_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
 
-    if choice == "Home":
-    	st.write("Go to the About section from the sidebar to learn more about it.")
-    	image_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
-    	if image_file is not None:
-    		image = Image.open(image_file)
-    		if st.button("Process"):
-    			detect_bugs(image=image)
-    			#st.image(result_img, use_column_width = True)
-    			#st.success("Found {} faces\n".format(len(result_faces)))
+    if image_file is not None:
+    	image = Image.open(image_file)
+    	if st.button("Process"):
+    		utils.detect_bugs(image=image)
 
-    elif choice == "About":
-    	about()
-
+    #utils.zipdir('onion', zipfile.ZipFile('onion.zip', 'w'))
 
 if __name__ == "__main__":
     main()
---
+++ webapp/utils.py
@@ -0,0 +1,91 @@
+import cv2
+import streamlit as st
+import numpy as np
+import base64
+import zipfile
+import os
+
+def detect_bugs(image):
+    #Gray scale and blur the image
+    img = np.array(image.convert('RGB'))
+    imgGray = np.array(image.convert('L'))
+    imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
+    height, width, channels = img.shape
+
+    # Setup SimpleBlobDetector parameters.
+    params = cv2.SimpleBlobDetector_Params()
+
+    # Filter by Area.
+    params.filterByArea = True
+    params.minArea = 300
+    #params.maxArea = 3000
+
+    # Filter by Convexity
+    params.filterByConvexity = True
+    params.minConvexity = 0.01
+
+    # Filter by Inertia
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.01
+
+    # Create a detector with the parameters
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    #detector = cv2.SimpleBlobDetector_create()
+    keypoints = detector.detect(imgBlur)
+    imgKeyPoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    # Crop out keypoints
+    #os.mkdir("images")
+    for keypoint in keypoints:
+        filename = 1
+        x = int(keypoint.pt[0])
+        y = int(keypoint.pt[1])
+        size = int(keypoint.size)
+        crop = img[max(1,y-2*size): min(height-1,y+2*size), max(1,x-2*size): min(width-1,x+2*size)]
+        st.image(crop, use_column_width = False)
+        #cv2.imwrite(str(filename) + ".jpg", crop)
+        
+    return imgKeyPoints
+
+#stuff to set background image
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_bg_image(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+"""
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
+    zip_path = 'test.zip'
+    zipf = zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED)
+    zipdir('onion/', zipf)
+    zipf.close()
+
+    with open(zip_path, "rb") as f:
+        bytes = f.read()
+        b64 = base64.b64encode(bytes).decode()
+        href = f'<a href="data:file/zip;base64,{b64}" download=\'{filename}.zip\'>\
+            Click to download\
+            </a>'
+    st.sidebar.markdown(href, unsafe_allow_html=True)
+"""
